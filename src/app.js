94d2fd5610ea443e67d77b5a6ced372accbb09c9
Xin-hai <aa1940358283@163.com>
2023-08-19 02:40:58 +0800

    成功引入sass，编译成功

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Button from "./button.vue";
+
+Vue.config.productionTip = false
+
+
+Vue.component('g-button', Button)
+
+new Vue({
+    el:'#app',
+    data: {
+
+    }
+})
