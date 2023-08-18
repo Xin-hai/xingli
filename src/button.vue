0547cfe6b07896fbcbc8ac425870d420453c0b47
Xin-hai <aa1940358283@163.com>
2023-08-19 04:46:11 +0800

    验证iconPosition只能为right和left其中一个。

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,17 @@
 <script>
 
 export default {
-  props: ['icon', 'iconPosition']
+  props:{
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator (value){
+        console.log(value);
+        return value !== 'left' && value !== 'right' ? false : true;
+      }
+    }
+  }
 }
 </script>
 
