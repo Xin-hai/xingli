b651db1561548c7f97754761467a04bea5e1d009
Xin-hai <aa1940358283@163.com>
2023-08-19 19:24:31 +0800

    基本完成g-button-group

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Button from "./button.vue";
 import icon from './icon.vue'
+import ButtonGroup from "./button-group.vue";
 
 Vue.config.productionTip = false
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', icon)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
     el:'#app',
--- src/button.vue
+++ src/button.vue
@@ -37,48 +37,21 @@ export default {
 }
 
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  vertical-align: middle;
-
-  &:hover {
-    border-color: var(--border-color-hover);
-  }
-
-  &:active {
-    background: var(--button-active-bg);
-  }
-
-  &:focus {
-    outline: none;
-  }
+  font-size: var(--font-size);height: var(--button-height);
+  padding: 0 1em;border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);background: var(--button-bg);
+  display: inline-flex;justify-content: center;
+  align-items: center;vertical-align: middle;
+  &:hover {border-color: var(--border-color-hover);}
+  &:active {background: var(--button-active-bg);}
+  &:focus {outline: none;}
   > .icon{
-    order: 1;
-    margin-right: .1em;
-  }
-  > .content{
-    order: 2;
-  }
+    order: 1;margin-right: .1em;}
+  > .content{order: 2;}
   &.icon-right{
-    > .icon{
-      order: 2;
-      margin-right: 0;
-      margin-left: .1em;
-    }
-    > .content {
-      order: 1;
-    }
+    > .icon{order: 2;margin-right: 0;margin-left: .1em;}
+    > .content {order: 1;}
   }
-  .loading{
-    animation: spin 2s infinite linear;
-  }
+  .loading{animation: spin 2s infinite linear;}
 }
-
 </style>
