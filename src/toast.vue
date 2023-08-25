15f40307ab1f36083a58d1d68231480cb7110af2
Xin-hai <aa1940358283@163.com>
2023-08-25 10:21:07 +0800

    添加动画

--- src/toast.vue
+++ src/toast.vue
@@ -91,7 +91,12 @@ $font-size: 14px;
 $toast-min-height: 40px;
 $toast-bg: #f0f9eb;
 $toast-color: #67c23a;
+@keyframes fade-in {
+  0% {opacity: 0}
+  100%{opacity: 1}
+}
 .toast {
+  animation: fade-in 1s;
   display: flex;align-items: center;
   position: fixed;
   font-size: $font-size;min-height: $toast-min-height;line-height: 1.8;
