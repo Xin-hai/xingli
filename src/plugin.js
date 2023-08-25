d48c6c637f3e957f73742b438a2b8207464d8af8
Xin-hai <aa1940358283@163.com>
2023-08-25 10:21:55 +0800

    解决currentToast的小bug

--- src/plugin.js
+++ src/plugin.js
@@ -2,13 +2,14 @@ import Toast from './toast.vue'
 
 
 // 动态创建 组件
-function createToast({Vue,message, propsData}){
+function createToast({Vue,message, propsData,onClose}){
     let Constructor = Vue.extend(Toast)
     let toast = new Constructor({
         propsData: propsData
     })
     toast.$slots.default = [message]
     toast.$mount()
+    toast.$on('beforeClose', onClose)
     document.body.appendChild(toast.$el)
     return toast
 }
@@ -20,7 +21,12 @@ export default {
           if(currentToast){
               currentToast.close()
           }
-           currentToast = createToast({Vue,message,propsData: toastOptions})
+           currentToast = createToast({
+               Vue,
+               message,
+               propsData: toastOptions,
+               onClose: ()=>{currentToast=null}
+           })
         }
     }
 }
--- src/toast.vue
+++ src/toast.vue
@@ -67,6 +67,7 @@ export default {
     },
     close(){
       this.$el.remove()
+      this.$emit('beforeClose')
       this.$destroy()
     },
     onClickClose(){
